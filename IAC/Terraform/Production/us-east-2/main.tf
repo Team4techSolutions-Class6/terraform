3554c03c57a6a3c846f2376bf26ca04d13af345b
Team4tech Solutions <team4techsolutions.inc@gmail.com>
2025-02-10 01:14:58 -0500

    Create main.tf

---
+++ IAC/Terraform/Production/us-east-2/main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.0.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+module "vpc" {
+  source               = "../modules/vpc"
+  vpc_name             = "Test"
+  cidr_block           = "172.24.0.0/16"
+  public_subnet_cidr    = "172.24.0.0/24"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  availability_zone = "us-east-2b"
+}
+
+module "ec2_instance" {
+  source        = "../modules/ec2"
+  ami_id        = "ami-0e86e20dae9224db8"
+  instance_type = "t2.micro"
+  instance_name = "Jenkins"
+  subnet_id     = module.vpc.public_subnet_id 
+  key_name      = "team4techsolutions2"  
+}
+
+
+
