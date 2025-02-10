0c1251cabef079e8b6bd8c753f0d15bb2ad5d0f5
Team4tech Solutions <team4techsolutions.inc@gmail.com>
2025-02-10 01:17:25 -0500

    Create main.tf

---
+++ IAC/Terraform/Modules/vpc/main.tf
@@ -0,0 +1,20 @@
+resource "aws_vpc" "main" {
+  cidr_block           = var.cidr_block
+  enable_dns_support   = var.enable_dns_support
+  enable_dns_hostnames = var.enable_dns_hostnames
+
+  tags = {
+    Name = var.vpc_name
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidr
+  availability_zone       = var.availability_zone
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.vpc_name}-public-subnet"
+  }
+}
