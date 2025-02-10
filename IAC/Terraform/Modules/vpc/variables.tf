b1c81842a008500118d4fca42ebd3f95d19cb498
Team4tech Solutions <team4techsolutions.inc@gmail.com>
2025-02-10 01:17:58 -0500

    Create variables.tf

---
+++ IAC/Terraform/Modules/vpc/variables.tf
@@ -0,0 +1,32 @@
+variable "vpc_name" {
+  description = "Name of the VPC"
+  type        = string
+}
+
+variable "cidr_block" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR block for the public subnet"
+  type        = string
+}
+
+variable "enable_dns_support" {
+  description = "Enable DNS support in the VPC"
+  type        = bool
+  default     = true
+}
+
+variable "enable_dns_hostnames" {
+  description = "Enable DNS hostnames in the VPC"
+  type        = bool
+  default     = true
+}
+
+variable "availability_zone" {
+  description = "Availability Zone for the subnet"
+  type        = string
+  default     = ""
+}
