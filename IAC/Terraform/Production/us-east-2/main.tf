7cfe601abbef7682f5b57fb2b9d75ab2e3e58e73
Team4tech Solutions <team4techsolutions.inc@gmail.com>
2025-02-10 02:45:52 -0500

    Update main.tf

--- IAC/Terraform/Production/us-east-2/main.tf
+++ IAC/Terraform/Production/us-east-2/main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 }
 
 module "vpc" {
-  source               = "../../modules/vpc"
+  source               = "github.com/Team4techSolutions-Class6/modules//vpc"
   vpc_name             = "Test"
   cidr_block           = "172.24.0.0/16"
   public_subnet_cidr    = "172.24.0.0/24"
@@ -24,7 +24,7 @@ module "vpc" {
 }
 
 module "ec2_instance" {
-  source        = "../../modules/ec2"
+  source        = "github.com/Team4techSolutions-Class6/modules//ec2"
   ami_id        = "ami-0e86e20dae9224db8"
   instance_type = "t2.micro"
   instance_name = "Jenkins"
